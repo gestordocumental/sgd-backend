82970a00c3b7d95aadba87d642c2143075f21036
juancastrong <juancahawk@gmail.com>
2026-04-17 00:59:47 -0500

    feat (#25)
    * feat
    
    * feat
    
    * feat

--- services/metadata-extractor-service/start.sh
+++ services/metadata-extractor-service/start.sh
@@ -2,4 +2,5 @@
 set -e
 
 echo ">>> Starting metadata-extractor-service..."
+echo ">>> ENV CHECK: STORAGE_BUCKET=${STORAGE_BUCKET} STORAGE_ENDPOINT=${STORAGE_ENDPOINT} STORAGE_ACCESS_KEY=${STORAGE_ACCESS_KEY:+SET}"
 exec node dist/main
