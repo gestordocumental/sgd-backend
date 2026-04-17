070aa19931148c16962324370aec7d3d7a62dd67
ISY99907 <juancahawk@gmail.com>
2026-04-17 03:34:52 -0500

    feat

---
+++ services/auth-service/jest.config.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: 'src',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: '../coverage',
+    testEnvironment: 'node',
+    setupFiles: ['reflect-metadata'],
+    forceExit: true,
+};
+exports.default = config;
+//# sourceMappingURL=jest.config.js.map
