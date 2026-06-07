27bc240ae42168bdcb4a8a32fbce6fa16484e944
ISY99907 <juancahawk@gmail.com>
2026-06-07 00:05:35 -0500

    feat

--- packages/common/src/tracing/tracing.ts
+++ packages/common/src/tracing/tracing.ts
@@ -1,7 +1,7 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { Resource } from '@opentelemetry/resources';
+import { resourceFromAttributes } from '@opentelemetry/resources';
 import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 
 let _sdk: NodeSDK | undefined;
@@ -23,7 +23,7 @@ export function initTracing(serviceName: string): void {
   const exporter = new OTLPTraceExporter({ url: `${endpoint}/v1/traces` });
 
   _sdk = new NodeSDK({
-    resource: new Resource({ [ATTR_SERVICE_NAME]: serviceName }),
+    resource: resourceFromAttributes({ [ATTR_SERVICE_NAME]: serviceName }),
     traceExporter: exporter,
     instrumentations: [
       getNodeAutoInstrumentations({
